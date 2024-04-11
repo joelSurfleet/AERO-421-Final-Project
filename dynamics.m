37498624a575fded05ec683d5a44d3a86114fd14
joelSurfleet <159580976+joelSurfleet@users.noreply.github.com>
2024-04-10 20:15:47 -0700

    Update dynamics.m

--- dynamics.m
+++ dynamics.m
@@ -5,7 +5,7 @@ function domega = dynamics(T, J, omega)
 
 % Using the Euler Equations and T = 0
 
-wXdot = (-(J(3,3)-J(2,2))*omega(2)*omega(3))/J(1,1);
-wYdot = (-(J(1,1)-J(3,3))*omega(1)*omega(3))/J(2,2);
-wZdot = (-(J(2,2)-J(1,1))*omega(1)*omega(2))/J(3,3);
+wXdot = (-(J(3,3)-J(2,2))*omega(2)*omega(3))/J(1,1) + T;
+wYdot = (-(J(1,1)-J(3,3))*omega(1)*omega(3))/J(2,2) + T;
+wZdot = (-(J(2,2)-J(1,1))*omega(1)*omega(2))/J(3,3) + T;
 domega = [wXdot; wYdot; wZdot];
